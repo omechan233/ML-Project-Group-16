b309a2c4681782ad5623c6ee90b16f4116a12059
omechan233 <31775045+omechan233@users.noreply.github.com>
2021-12-01 19:22:04 +0800

    Add files via upload

---
+++ outlier_remover.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+
+
+# data is the dataset read by pandas
+# cols is the columns you want to remove outliers
+# for this project, cols is always ['Duration', 'NumSubscribers', 'ViewCount']
+# the return value is cleaned pandas DataFrame
+def outlier_removal(data, cols):
+    data.boxplot(cols)
+    plt.show()
+    for i in cols:
+        q75, q25 = np.percentile(data.loc[:, i], [75, 25])
+        intr_qr = q75 - q25
+
+        _max = q75 + (1.5 * intr_qr)
+        _min = q25 - (1.5 * intr_qr)
+
+        data.loc[data[i] < _min, i] = np.nan
+        data.loc[data[i] > _max, i] = np.nan
+
+    outliers = data.isnull().sum()
+    row = len(data.index)
+    print(outliers)
+    data = data.dropna(axis=0)
+    row -= len(data.index)
+    print(str(row) + " outliers dropped.")
+    return data
+
+
+def main():
+    cols = ['Duration', 'NumSubscribers', 'ViewCount']
+    data = pandas.read_csv("data_youtube.csv")
+    outlier_removal(data, cols)
+
+
+main()
